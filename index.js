c8874ae0d83ceee539ca8e0271a7949f518c0d8f
Michael Jaser <michael.jaser@peerigon.com>
2018-06-15 18:15:29 +0200

    Fix issue with unassigning

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
+"use strict";
+
+function assignSender(context) {
+  context.github.issues.addAssigneesToIssue(context.issue({
+    assignees: [context.payload.sender.login]
+  }));
+}
+
 module.exports = (robot) => {
 
-  robot.on(["issues.unassigned"], async context => {
-    context.github.issues.addAssigneesToIssue(context.issue({
-      assignees: [context.payload.sender.login]
-    }));
-  });
-
-  robot.on(["issues.opened", "issues.edited"], async context => {
+  robot.on(["issues.opened", "issues.edited", "issues.unassigned"], async context => {
     if (context.payload.issue.assignees.length === 0) {
-      context.github.issues.addAssigneesToIssue(context.issue({
-        assignees: [context.payload.sender.login]
-      }));
+      assignSender(context);
     }
   });
-  
+
 };
